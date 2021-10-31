bdb82b80ad0a458ea7e67ea12aa5432292539424
明梦爽 <2972214568@qq.com>
2021-10-31 11:28:41 +0800

    完成登录功能

--- src/router.js
+++ src/router.js
@@ -1,21 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login.vue'
-import Home from './components/Home.vue'
-import Welcome from './components/Welcome.vue'
-import Users from './components/user/Users.vue'
 
 Vue.use(Router)
 
 const router = new Router({
   routes: [
     { path: '/', redirect: '/login' },
-    { path: '/login', component: Login },
-    { path: '/home', component: Home, redirect:'/welcome',children: [{
-      path: '/welcome', component: Welcome
-    },{
-      path: '/users', component: Users
-    }] }
+    { path: '/login', component: Login }
   ]
 })
 
